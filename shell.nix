e6b31fe30a9db0ceb60e05abcd49e42d217f14f8
Yui Yukihira <yuiyukihira@pm.me>
2022-08-19 18:46:51 +0100

    Add tools to nix-shell

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
 pkgs.mkShell {
-    nativeBuildInputs = [ pkgs.rustup ];
+    nativeBuildInputs = [ pkgs.rustup pkgs.gnumake pkgs.cargo-edit pkgs.bacon pkgs.crate2nix ];
 }
